acc9f26e96d098e3baa26b13f8911b8248580d74
David Underwood <david.underwood@jadedpixel.com>
2011-05-18 14:33:33 -0400

    fixed lookup syntax for webhook events

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -11,7 +11,7 @@ class HomeController < ApplicationController
     # get 3 products
     #@products = get_products
     
-    @webhook_events = WebhookEvent.find(:all, :params => {:limit => 10})
+    @webhook_events = WebhookEvent.find(:all)
 
     # get latest 3 orders
     @orders   = ShopifyAPI::Order.find(:all, :params => {:limit => 3, :order => "created_at DESC" })
