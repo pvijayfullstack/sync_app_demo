475299a9423345b29fd2ec4df2af39bfda0cee39
David Underwood <david.underwood@jadedpixel.com>
2011-05-18 14:35:42 -0400

    fixed webhook syntax

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -1,17 +1,17 @@
 class WebhookController < ApplicationController
 
   def product_new
-    event = webhook_event.new(:type => "product new", :description => request.body)
+    event = WebhookEvent.new(:type => "product new", :description => request.body)
     event.save
   end
 
   def product_updated
-    event = webhook_event.new(:type => "product update", :description => request.body)
+    event = WebhookEvent.new(:type => "product update", :description => request.body)
     event.save
   end
 
   def product_deleted
-    event = webhook_event.new(:type => "product delete", :description => request.body)
+    event = WebhookEvent.new(:type => "product delete", :description => request.body)
     event.save
   end
 
