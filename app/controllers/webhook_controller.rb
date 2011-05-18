51406f475eac52806283f1610f2465ced9982dcf
David Underwood <david.underwood@jadedpixel.com>
2011-05-18 14:52:49 -0400

    sorting out column rename in code

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -1,17 +1,17 @@
 class WebhookController < ApplicationController
 
   def product_new
-    event = WebhookEvent.new(:type => "product new", :description => request.body)
+    event = WebhookEvent.new(:event_type => "product new", :description => request.body)
     event.save
   end
 
   def product_updated
-    event = WebhookEvent.new(:type => "product update", :description => request.body)
+    event = WebhookEvent.new(:event_type => "product update", :description => request.body)
     event.save
   end
 
   def product_deleted
-    event = WebhookEvent.new(:type => "product delete", :description => request.body)
+    event = WebhookEvent.new(:event_type => "product delete", :description => request.body)
     event.save
   end
 
