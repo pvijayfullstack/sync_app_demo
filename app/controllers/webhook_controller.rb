909196f2b306b52c5ad6a2dfba31c2b93fbef33d
David Underwood <david.underwood@jadedpixel.com>
2011-05-18 14:56:45 -0400

    adding to_s to webhook event descs

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -1,17 +1,17 @@
 class WebhookController < ApplicationController
 
   def product_new
-    event = WebhookEvent.new(:event_type => "product new", :description => request.body)
+    event = WebhookEvent.new(:event_type => "product new", :description => request.body.to_s)
     event.save
   end
 
   def product_updated
-    event = WebhookEvent.new(:event_type => "product update", :description => request.body)
+    event = WebhookEvent.new(:event_type => "product update", :description => request.body.to_s)
     event.save
   end
 
   def product_deleted
-    event = WebhookEvent.new(:event_type => "product delete", :description => request.body)
+    event = WebhookEvent.new(:event_type => "product delete", :description => request.body.to_s)
     event.save
   end
 
