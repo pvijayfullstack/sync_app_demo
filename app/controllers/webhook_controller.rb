c53cd6dbadcded4e3f5a88852eeb866e8acedd75
David Underwood <david.underwood@jadedpixel.com>
2011-05-18 14:59:04 -0400

    more fiddling with webhook descriptions

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -1,17 +1,17 @@
 class WebhookController < ApplicationController
 
   def product_new
-    event = WebhookEvent.new(:event_type => "product new", :description => request.body.to_s)
+    event = WebhookEvent.new(:event_type => "product new", :description => ActiveSupport::JSON.decode(request.body).to_s)
     event.save
   end
 
   def product_updated
-    event = WebhookEvent.new(:event_type => "product update", :description => request.body.to_s)
+    event = WebhookEvent.new(:event_type => "product update", :description => ActiveSupport::JSON.decode(request.body).to_s)
     event.save
   end
 
   def product_deleted
-    event = WebhookEvent.new(:event_type => "product delete", :description => request.body.to_s)
+    event = WebhookEvent.new(:event_type => "product delete", :description => ActiveSupport::JSON.decode(request.body).to_s)
     event.save
   end
 
