d14f1aebf94388ae2efaeeef600cc66a9b756761
David Underwood <david.underwood@jadedpixel.com>
2011-05-18 14:51:19 -0400

    removing a broken migration

--- db/migrate/20110518184251_webhook_type_column_name_change.rb
+++
@@ -1,9 +0,0 @@
-class WebhookTypeColumnNameChange < ActiveRecord::Migration
-  def self.up
-    change_column :webhook_events, :type, :event_type
-  end
-
-  def self.down
-    change_column :webhook_events, :event_type, :type
-  end
-end
