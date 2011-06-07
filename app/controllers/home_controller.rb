20756da85092610e6da034e6635cca9509e83d1e
David Underwood <david.underwood@jadedpixel.com>
2011-06-07 16:49:59 -0400

    Added shop stuff to home controller and improved the all_products method. Added webhook registration.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,27 +8,48 @@ class HomeController < ApplicationController
   end
   
   def index
+    if Shop.where(:name => ShopifyAPI::Shop.current.name).exists?
+      session[:shop] = Shop.where(:name => ShopifyAPI::Shop.current.name).first
+    else    
+      shop = Shop.new(:name => ShopifyAPI::Shop.current.name, :url => "http://#{ShopifyAPI::Shop.current.domain}", :installed => true)
+      shop.save
+      session[:shop] = shop
+      init_webhooks
+      get_products shop
+    end
     
     @webhook_events = WebhookEvent.limit(10).order('id DESC')
-    @products = Product.where('logical_delete IS NULL')
-
+    @products = Product.where(:logical_delete => nil, :shop_id => session[:shop].id)
   end
   
+  private
   
-  def get_products
-    limit = 40
+  def get_products(shop)
+    limit = 250
     all_products = Array.new
     products = ShopifyAPI::Product.find(:all, :params => {:limit => limit})
     all_products = all_products.concat products
     puts products.length
     while products.length == limit do
       since_id = products.last.id
-      puts since_id
-      puts products.length
       products = ShopifyAPI::Product.find(:all, :params => {:limit => limit, :since_id => since_id})
       all_products = all_products.concat products
     end
-    all_products
+    
+    all_products.each do |product|
+      unless Product.where('shopify_id = ?', product.id).exists?
+        Product.new(:name => product.title, :shopify_id => product.id, :shop => shop).save
+      end
+    end
+  end
+  
+  def init_webhooks
+    topics = ["products/create", "products/update", "products/delete"]
+    
+    topics.each do |topic|
+      webhook = ShopifyAPI::Webhook.create(:format => "json", :topic => topic, :address => "http://#{DOMAIN_NAMES[RAILS_ENV]}/webhooks/#{topic}")
+      raise "Webhook invalid: #{webhook.errors}" unless webhook.valid?
+    end
   end
   
 end
